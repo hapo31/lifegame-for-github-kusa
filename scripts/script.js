4d6c24d94e57ee10302c8eebb0a63b48d8c48255
happou31 <happo31@gmail.com>
2017-09-07 21:31:29 +0900

    右端列もマス目として有効化した ・足りないセルを追加で生成するようにした

--- scripts/script.js
+++ scripts/script.js
@@ -12,15 +12,26 @@
         "#196127"
     ];
 
-    const life = new LifeGame(7, 52);
+    const life = new LifeGame(7, 53);
 
     const elements = [];
-    const initField = new Array(7 * 54);
+    const initField = new Array(7 * 53);
 
 
     const gElements = fieldDoc.querySelectorAll("g");
     for (let y = 0; y < life.height; ++y) {
         const rect = gElements[y].getElementsByTagName("rect");
+        // 右端の列の足りない部分を生成する処理
+        while (rect.length < 7) {
+            const r = document.createElementNS("http://www.w3.org/2000/svg","rect");
+            r.setAttribute("class", "day");
+            r.setAttribute("width", 10);
+            r.setAttribute("height", 10);
+            r.setAttribute("x", -39);
+            r.setAttribute("y", 12 * rect.length);
+            r.setAttribute("fill", colors[0]);
+            rect[rect.length] = r;
+        }
         for (let x = 0; x < life.width; ++x) {
             elements.push(rect[x]);
             const v = rect[x].getAttribute("fill") !== "#ebedf0" ? 1 : 0;
@@ -35,7 +46,7 @@
                 for (let x = 0; x < life.width; ++x) {
                     if (life.field[y * life.width + x] === 1) {
                         const rnd = Math.floor(Math.random() * (colors.length - 1)) + 1;
-                        elements[y * life.width + x].setAttribute("fill", colors[rnd === 0 ? 1 : rnd]);
+                        elements[y * life.width + x].setAttribute("fill", colors[rnd]);
                     } else {
                         elements[y * life.width + x].setAttribute("fill", colors[0]);
                     }
